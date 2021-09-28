d2646d7d70eeefeeddea34f466a1b493e3099d11
Fabian Sorger <ITN250462@qualifizierung.at>
2021-09-28 14:36:07 +0200

    Load Question auf Liste laden? geändert :)

--- Einkaufliste/Einkaufliste/MainPage.xaml.cs
+++ Einkaufliste/Einkaufliste/MainPage.xaml.cs
@@ -63,7 +63,7 @@ namespace Einkaufliste
 
         async private void LadeneEinkaufsliste()
         {
-            if (await DisplayAlert("Question?", "Load Messages?", "Yes", "No"))
+            if (await DisplayAlert("Frage?", "Liste laden?", "Ja", "Nein"))
             {
                 string jsonString = Application.Current.Properties[MainPage.einkaufslisteKey] as string;
                 einkaufsliste = JsonConvert.DeserializeObject<ObservableCollection<Artikel>>(jsonString);
